bbffcaddf83b90b25420fd0c2b7029751dcd6f05
crijanp <srij89995@gmail.com>
2025-09-16 20:57:08 +0545

    configure multi-guard authentication sytem

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,21 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        
+        'brand' => [
+            'driver' => 'session',
+            'provider' => 'brands',
+        ],
+        
+        'influencer' => [
+            'driver' => 'session',
+            'provider' => 'influencers',
+        ],
+        
+        'admin' => [
+            'driver' => 'session',
+            'provider' => 'admins',
+        ],
     ],
 
     /*
@@ -63,6 +78,21 @@ return [
         'users' => [
             'driver' => 'eloquent',
             'model' => env('AUTH_MODEL', App\Models\User::class),
+        ],
+        
+        'brands' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Brand::class,
+        ],
+        
+        'influencers' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Influencer::class,
+        ],
+        
+        'admins' => [
+            'driver' => 'eloquent',
+            'model' => App\Models\Admin::class,
         ],
 
         // 'users' => [
@@ -94,6 +124,27 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        
+        'brands' => [
+            'provider' => 'brands',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        
+        'influencers' => [
+            'provider' => 'influencers',
+            'table' => 'password_reset_tokens',
+            'expire' => 60,
+            'throttle' => 60,
+        ],
+        
+        'admins' => [
+            'provider' => 'admins',
+            'table' => 'password_reset_tokens',
             'expire' => 60,
             'throttle' => 60,
         ],
