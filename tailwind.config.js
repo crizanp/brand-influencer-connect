f3afcaea0a6bb2d51377915bbfabd63083532e0f
crijanp <srij89995@gmail.com>
2025-09-16 20:53:04 +0545

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#f0f9ff',
+          500: '#3b82f6',
+          600: '#2563eb',
+          700: '#1d4ed8',
+        },
+        secondary: {
+          50: '#fdf4ff',
+          500: '#a855f7',
+          600: '#9333ea',
+          700: '#7c3aed',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
