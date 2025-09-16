fe9a3a987e2d53b34d18f7183838150bfc69227c
crijanp <srij89995@gmail.com>
2025-09-16 20:53:49 +0545

    creation of  the database migrations for brands, influencers, and admins

---
+++ database/migrations/2025_09_16_150807_create_brands_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('contact_person')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('company_description')->nullable();
+            $table->string('website')->nullable();
+            $table->string('industry')->nullable();
+            $table->string('logo')->nullable();
+            $table->enum('status', ['active', 'inactive', 'pending'])->default('pending');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
